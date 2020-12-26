f32db9af572c2712aa9279b94ff036b3f6fe822b
SavileLee <SavileLee@users.noreply.github.com>
2020-12-26 15:03:16 +0800

    FilterRules

--- Scripts/YouTube.js
+++ Scripts/YouTube.js
@@ -4,10 +4,10 @@ YouTube ADS Remove
 Surge4.0:
 [Script]
 # 如果您是 YouTube Premium，请不要开启此模块。
-youtube_ad = type=http-request,pattern=^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A),script-path=https://raw.githubusercontent.com/SavileLee/Profiles/Rules/FilterRules/X/Scripts/YouTube.js
+youtube_ad = type=http-request,pattern=^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A),script-path=https://raw.githubusercontent.com/SavileLee/FilterRules/Rules/Scripts/YouTube.js
 
 QX 1.0.0:
-^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A) url script-request-header https://raw.githubusercontent.com/SavileLee/Profiles/Rules/FilterRules/X/Scripts/YouTube.js
+^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A) url script-request-header https://raw.githubusercontent.com/SavileLee/FilterRules/Rules/Scripts/YouTube.js
 
 Surge & QX Mitm = *.googlevideo.com
 */
